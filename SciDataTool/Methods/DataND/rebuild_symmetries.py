edeb3bd5c0c970cb2ddd300915869c95f0329c10
Helene Toubin <helene.toubin@eomys.com>
2020-09-28 10:33:47 +0200

    [CC] Add csv of classes

---
+++ SciDataTool/Methods/DataND/rebuild_symmetries.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from SciDataTool.Functions.symmetries import rebuild_symmetries as rebuild_symmetries_fct
+
+def rebuild_symmetries(self, values, axes_list):
+    """Reconstructs the field of a Data object taking symmetries into account
+    Parameters
+    ----------
+    self: Data
+        a Data object
+    values: ndarray
+        ndarray of a field
+    axes_list: list
+        a list of RequestedAxis objects
+    Returns
+    -------
+    ndarray of the reconstructed field
+    """
+    for axis_requested in axes_list:
+        # Rebuild symmetries
+        if(
+            axis_requested.transform != "fft"
+            and axis_requested.indices is None
+            and axis_requested.corr_name in self.symmetries.keys()
+        ):
+            values = rebuild_symmetries_fct(
+                values, axis_requested.index, self.symmetries.get(axis_requested.corr_name)
+            )
+    return values    
