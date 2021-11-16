231837059128836c2c8121b88ed3cc1bdf9d3fdc
BaptisteMadery14 <baptiste.madery@eomys.com>
2021-11-16 09:10:03 +0100

    [CO] Adding items inside unit dict

--- Tests/GUI/dev_GUI.py
+++ Tests/GUI/dev_GUI.py
@@ -1,7 +1,7 @@
 from PySide2.QtWidgets import *
 from SciDataTool.Classes.DataLinspace import DataLinspace
 from SciDataTool.Classes.DataTime import DataTime
-from SciDataTool import Norm_ref
+from SciDataTool.Classes.VectorField import VectorField
 import numpy as np
 
 
@@ -29,3 +29,15 @@ if __name__ == "__main__":
     # Field.plot("time", "angle[0]", "z[2]", unit="T", zmax="10")
     # Field.plot("wavenumber", "freqs", "z[2]", unit="T", zmax="50")
     # Field.plot("freqs", "wavenumber", "z=mean", unit="T", zmax="50")
+    field_3d2 = np.zeros((11, 21, 3))
+    for i in range(3):
+        field_3d2[:, :, i] = (i + 2) * field
+
+    Field2 = DataTime(
+        name="Second component",
+        symbol="B_r",
+        unit="T",
+        axes=[X, Y, Z],
+        values=field_3d2,
+    )
+    Vecfield = VectorField(components={"comp_x": Field, "comp_y": Field2})
