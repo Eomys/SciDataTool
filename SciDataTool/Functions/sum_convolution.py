c6a147ae679b75ab575e849e7c1408c84ffad566
EmileDvs <58598539+EmileDvs@users.noreply.github.com>
2021-12-01 15:23:30 +0100

    [BC] update unique_tol args

--- SciDataTool/Functions/sum_convolution.py
+++ SciDataTool/Functions/sum_convolution.py
@@ -57,7 +57,7 @@ def get_conv_indices(freqs1, freqs2, f_min, f_max, tol_freq):
     I0 = np.logical_and.reduce((freqs >= f_min, freqs <= f_max))
 
     # Keep only unique rows in order array
-    result = unique_tol(
+    freqs_un, I1 = unique_tol(
         freqs[I0],
         return_inverse=True,
         return_index=True,
@@ -65,8 +65,6 @@ def get_conv_indices(freqs1, freqs2, f_min, f_max, tol_freq):
         is_abs_tol=True,
         tol=tol_freq,
     )
-    freqs_un = result["b"]
-    I1 = result["Ib"]
 
     # Find indices of positive frequencies to be kept
     I0a = np.where(I0)[0]
@@ -170,7 +168,7 @@ def get_sum_indices(freqs1, freqs2, tol_freq):
     freqs = np.concatenate((freqs1, freqs2), axis=0)
 
     # Get unique orders
-    res = unique_tol(
+    freqs_un, I0b = unique_tol(
         freqs,
         return_index=True,
         return_inverse=True,
@@ -178,9 +176,6 @@ def get_sum_indices(freqs1, freqs2, tol_freq):
         is_abs_tol=True,
         tol=tol_freq,
     )
-
-    freqs_un = res["b"]
-    I0b = res["Ib"]
 
     return freqs_un, I0b
 
