bf826c8c7d466ec8060bbc75abfd64152b918aed
BaptisteMadery14 <baptiste.madery@eomys.com>
2022-03-16 10:42:28 +0100

    [NF] Adding test on plot_3D_animate

--- Tests/GUI/UI/test_animate.py
+++ Tests/GUI/UI/test_animate.py
@@ -5,8 +5,7 @@ from PySide2 import QtWidgets
 import sys
 
 from tenacity import sleep
-from Tests.GUI.VectorField import VecField
-from SciDataTool.GUI import DATA_DIR
+from Tests.GUI import Field
 
 
 class TestGUI(object):
@@ -18,8 +17,8 @@ class TestGUI(object):
         else:
             cls.app = QtWidgets.QApplication.instance()
 
-        cls.VecField = VecField
-        cls.UI = cls.VecField.plot(is_show_fig=False, is_create_appli=False)
+        cls.Field = Field
+        cls.UI = cls.Field.plot(is_show_fig=False, is_create_appli=False)
 
     @pytest.mark.gui
     def test_check_animation_2D(self):
@@ -40,6 +39,28 @@ class TestGUI(object):
         # Deleting gif for future test
         remove(self.UI.w_plot_manager.gif)
 
+    @pytest.mark.gui
+    def test_check_animation_3D(self):
+        """Testing that when the user click on the animate button, then animation is created and stored at the right place"""
+
+        # Selecting an axis on Y to go 3D
+        self.UI.w_plot_manager.w_axis_manager.w_axis_2.c_axis.setCurrentIndex(1)
+
+        # Selection WSliceOperator + emitting signal to generate animation
+        w_slice_angle = self.UI.w_plot_manager.w_axis_manager.w_slice_op[0]
+        assert w_slice_angle.axis_name == "z"
+        w_slice_angle.b_animate.clicked.emit()
+        sleep(60)
+
+        # Making sure that the gif is saved at the right place
+        assert isfile(self.UI.w_plot_manager.gif) == True
+
+        # Closing the animation
+        self.UI.close()
+
+        # Deleting gif for future test
+        remove(self.UI.w_plot_manager.gif)
+
 
 if __name__ == "__main__":
     a = TestGUI()
@@ -47,5 +68,8 @@ if __name__ == "__main__":
 
     # Testing that the gif is created and located at the right path
     a.test_check_animation_2D()
+
+    # Testing that the gif is created and located at the right path
+    # a.test_check_animation_3D()
 
     print("Done")
