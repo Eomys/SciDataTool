c661a33db14670398ebfcc95cf17ee3cd8542179
BaptisteMadery14 <baptiste.madery@eomys.com>
2022-03-24 10:08:21 +0100

    [CC] Cleaning get_file_name method (only returning data.name)

--- SciDataTool/GUI/WPlotManager/WPlotManager.py
+++ SciDataTool/GUI/WPlotManager/WPlotManager.py
@@ -130,7 +130,7 @@ class WPlotManager(Ui_WPlotManager, QWidget):
 
         # Recovering the name of the gif (changing name for animated axis)
         if self.default_file_path is None:
-            gif_name = self.get_file_name(is_gif=True, animated_axis=animated_axis)
+            gif_name = self.get_file_name()
         else:
             gif_name = self.default_file_path
 
@@ -260,24 +260,10 @@ class WPlotManager(Ui_WPlotManager, QWidget):
         """
         self.updatePlot.emit()
 
-    def get_file_name(self, is_gif=False, animated_axis=""):
-        """Method that create the name of the file according to the input of the user"""
-        param_list = [
-            *self.w_axis_manager.get_axes_selected(),
-            *self.w_axis_manager.get_operation_selected(),
-        ]
-        if None in param_list:
-            param_list.remove(None)
+    def get_file_name(self):
+        """Method that create the name of the file with the name of the field selected"""
 
-        # Changing name of animated axis + unit of field for gif
-        if is_gif:
-            for idx_ax in range(len(param_list)):
-                ax = param_list[idx_ax]
-                if animated_axis.split("[")[0] in ax:
-                    param_list[idx_ax] = ax.split("=")[0] + "=animated"
-
-        data_unit = "[" + self.w_range.get_field_selected()["unit"] + "]"
-        file_name = self.data.name + data_unit  # + "_" + "_".join(param_list)
+        file_name = self.data.name
         file_name = file_name.replace("{", "[").replace("}", "]").replace(".", ",")
 
         return file_name
